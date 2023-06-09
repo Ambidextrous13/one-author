423bfaf319f5c2ce41d5d64e0e599b59d29a9691
ambidextrous13 <ambidextrous.1423@gmail.com>
2023-06-09 06:42:59 +0530

    solved conercase: Auto ID set not triggered

--- assets/src/js/one-author.js
+++ assets/src/js/one-author.js
@@ -36,18 +36,15 @@ import './one-author-ajax.js';
 
 		if ( nameElement && idElement ) {
 			setId( nameElement, idElement );
-			[ 'change', 'idChanged' ].forEach( ( event_ ) => {
-				nameElement.addEventListener( event_, ( event ) => {
+			if ( 1 < nameElement.length ) {
+				nameElement.addEventListener( 'change', ( event ) => {
 					const subjectedElement = event.target;
 					const optionNumber = subjectedElement.selectedIndex;
 					const option = subjectedElement[ optionNumber ];
 					setId( option, idElement );
-					if ( 1 < nameElement.length ) {
-						idElement.dispatchEvent( adminDemands );
-					}
-					disabledIt( submitBtn );
+					idElement.dispatchEvent( adminDemands );
 				} );
-			 } );
+			}
 		}
 	}()
 );
